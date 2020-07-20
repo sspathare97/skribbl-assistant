8208ae543018ab42d1ba821f9cc8a5ebb094afa2
sspathare97 <sspathare97@gmail.com>
2020-07-20 17:37:25 +0530

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# skribbl-assistant
+# Skribbl Assistant
 Fetches the Skribbl.io wordlist and displays clickable hints based on the current word's pattern.
+
+## How to use
+* Press ALT to enable/disable Skribbl Assistant
+* Click on a hint to submit it
+* Type in the chatbox to highlight hints.
+
+## Exact pattern matching: 
+* \_\_\_\_\_\_\_ will suggest catfish
+* \_\_ \_\_\_\_ will suggest Mr Bean
+* \_\_-\_\_ will suggest yo-yo  
+
+## Install as a Chrome Extension from <a href="https://chrome.google.com/webstore/detail/skribbl-assistant/ohniohajdcaicipofiohnkejhmdjhile" target="_blank">Chrome Web Store</a>
+[![Chrome Web Store](https://developer.chrome.com/webstore/images/ChromeWebStore_BadgeWBorder_v2_340x96.png)](https://chrome.google.com/webstore/detail/skribbl-assistant/ohniohajdcaicipofiohnkejhmdjhile)
+
+## Install as a User Script from <a href="https://greasyfork.org/en/scripts/407408-skribbl-assistant" target="_blank">Greasy Fork</a>
+
+## Notes
+* Since, it uses a fixed wordlist, it may not suggest new words if Skribbl.io updates the wordlist. Also, it will not work for custom words in private rooms
+* Don't click on multiple hints per second as rapidly submitting multiple words will result in getting kicked out by Skribbl.io
+
+## Screenshots
+1. Press ALT to enable
+![1](/screenshots/1.png)
+1. Type to highlight hints  
+![2](/screenshots/2.png)
+3. Highlighted hints with pattern
+![3](/screenshots/3.png)
+4. Highlighted hints with pattern
+![4](/screenshots/4.png)
+5. Guessed
+![5](/screenshots/5.png)
---
+++ skribbl-assistant/skribbl-assistant.js
@@ -0,0 +1,171 @@
+// ==UserScript==
+// @name         Skribbl Assistant
+// @version      1.0.0
+// @description  Fetches the Skribbl.io wordlist and displays clickable hints based on the current word's pattern.
+// @author       sspathare97
+// @match        https://skribbl.io/*
+// @grant        none
+// @namespace    https://greasyfork.org/en/users/668361-sspathare97
+// ==/UserScript==
+
+(async () => {
+  'use strict';
+
+  const formChat = document.getElementById('formChat');
+  const inputChat = document.getElementById('inputChat');
+  const currentWord = document.getElementById('currentWord');
+  const containerSidebar = document.getElementById('containerSidebar');
+  const boxMessages = document.getElementById('boxMessages');
+  const containerFreespace = document.getElementById('containerFreespace');
+
+  const refreshDelay = 500;
+  let wordList;
+  let currentWordVal = '';
+  let enabled = false;
+  let assistantPanel;
+  let hintBox;
+
+  const disabledText = 'Press ALT to enable Skribbl Assistant.';
+  const enabledText = 'Type to highlight hints. Press ALT to disable.';
+
+  const hintClick = (event) => {
+    inputChat.value = event.target.innerHTML;
+    formChat.dispatchEvent(
+      new Event('submit', {
+        bubbles: true,
+        cancelable: true,
+      })
+    );
+    assist();
+  };
+
+  const assist = (event, wordChanged = false) => {
+    const currentWordVal = currentWord.textContent;
+    let wordRegex = currentWordVal.replace(/_/g, '[^ -"]');
+    wordRegex = '"'.concat(wordRegex, '"');
+    wordRegex = new RegExp(wordRegex, 'g');
+
+    let hints = wordList.match(wordRegex);
+    if (!hints) {
+      hintBox.innerHTML = '<span style="color:black">Sorry, no hints available!</span>';
+      return;
+    } else {
+      hintBox.innerHTML = '<span style="color:black">Click on a hint to submit it: </span><br>';
+    }
+    hints = hints.map((hint) => {
+      return hint.substring(1, hint.length - 1);
+    });
+    hints = hints.sort((a, b) => {
+      return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+
+    if (window.Skribbl_Assistant_Debug && wordChanged) {
+      console.log({currentWordVal, hints});
+    }
+
+    const inputChatVal = inputChat.value;
+    hints.forEach((hint) => {
+      const hintSpan = document.createElement('a');
+      hintSpan.innerHTML = hint;
+      hintSpan.style.color = 'royalblue';
+      hintSpan.href = 'javascript:void(0);';
+      hintSpan.onclick = hintClick;
+      if (
+        inputChatVal &&
+        hint.toLowerCase().search(inputChatVal.toLowerCase()) !== -1
+      ) {
+        hintSpan.style.background = 'greenyellow';
+      }
+      hintBox.appendChild(hintSpan);
+      hintBox.appendChild(document.createTextNode(', '));
+    });
+    hintBox.removeChild(hintBox.lastChild);
+
+    boxMessages.scrollTop = boxMessages.scrollHeight;
+  };
+
+  const initialize = async () => {
+    try {
+      wordList = await fetch(
+        'https://api.npoint.io/91ac00bc3d335f00e13f'
+      ).then((response) => response.json());
+    } catch (e) {
+      await new Promise((resolve) => setTimeout(resolve, refreshDelay));
+      return initialize();
+    }
+
+    window.Skribbl_Assistant_Debug = false;
+
+    wordList = JSON.stringify(wordList);
+    wordList = wordList.substring(1, wordList.length - 1);
+
+    containerFreespace.style.display = 'none';
+    assistantPanel = document.createElement('p');
+
+    assistantPanel.innerHTML = `
+        <span style="font-size: larger">
+          <strong>
+            <a target="_blank" href="https://github.com/sspathare97/skribbl-assistant" style="color:green">Skribbl Assistant</a>
+            <span style="color:royalblue">by</span> 
+            <a target="_blank" href="https://sspathare97.com/" style="color:green">sspathare97</a>
+          </strong>
+        </span>
+        <br>
+      `;
+
+    assistantPanel.style = `
+        display: none;
+        background: rgb(238, 238, 238);
+        overflow-wrap: break-word;
+        border-radius: 2px;
+        border: 4px solid rgb(238, 238, 238);
+        width: 100%;
+        max-height: 250px;
+        overflow-y: auto;
+        color: rgb(57, 117, 206);
+    `;
+
+    hintBox = document.createElement('span');
+    assistantPanel.appendChild(hintBox);
+
+    containerSidebar.insertBefore(
+      assistantPanel,
+      containerSidebar.childNodes[0]
+    );
+
+    inputChat.setAttribute('placeholder', disabledText);
+
+    document.body.onkeyup = (event) => {
+      if (event.key === 'Alt') {
+        enabled = !enabled;
+        if (enabled) {
+          assistantPanel.style.display = '';
+          inputChat.setAttribute('placeholder', enabledText);
+        } else {
+          assistantPanel.style.display = 'none';
+          inputChat.setAttribute('placeholder', disabledText);
+        }
+      }
+    };
+
+    inputChat.onkeyup = assist;
+
+    setInterval(() => {
+      if (
+        currentWord &&
+        currentWord.textContent.indexOf('_') !== -1
+      ) {
+        if (currentWordVal !== currentWord.textContent) {
+          hintBox.style.display = '';
+          assist(undefined, true);
+          currentWordVal = currentWord.textContent;
+        }
+      } else {
+        hintBox.style.display = 'none';
+        currentWordVal = '';
+      }
+    }, refreshDelay);
+  };
+
+  initialize();
+})();
